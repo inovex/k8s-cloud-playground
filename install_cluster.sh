add906aed61cf731088cf5755c09efa706fe49a7
Henning Eggers <henning.eggers@inovex.de>
2022-07-29 14:54:26 +0200

    Fix cert manager deployment

--- install_cluster.sh
+++ install_cluster.sh
@@ -4,6 +4,8 @@ set -e
 LE_ACCOUNT_EMAIL=${1?"Please specify an email address for the letsencrypt account"}
 DNS_DOMAIN=$(terraform output --raw dns_domain)
 
+kubectl apply -k cluster-resources/crds/
+
 kubectl kustomize cluster-resources/ | \
     sed s/LE_ACCOUNT_EMAIL/${LE_ACCOUNT_EMAIL}/g | \
     sed s/DNS_DOMAIN/${DNS_DOMAIN}/g | \
